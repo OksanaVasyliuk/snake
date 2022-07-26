9c31972efeaf4456e25937a4a6f3fa9de4ce6d9c
Oksana Vasyliuk <o.vasyiuk@gmail.com>
2022-07-26 13:32:19 +0200

    hurra! snake is moving, eating fruit and grows with in next move and new fruit appears
    do not work: meeting with wall, bitting itself, command end

--- draft.py
+++ draft.py
@@ -1,4 +1,4 @@
-# from random import randrange
+from random import randrange
 
 # def print_board (table):
 #     print("Game board:")
@@ -40,24 +40,21 @@
 
 
 
+def game_board (size=10):
+    table = []
+    for a in range(size):
+        row = []
+        for b in range(size):
+            row.append(".")
+        table.append(row)
+    return table
 
-#     from random import randrange
-
-# def game_board (size=10):
-#     table = []
-#     for a in range(size):
-#         row = []
-#         for b in range(size):
-#             row.append(".")
-#         table.append(row)
-#     return table
-
-# def print_board (table):
-#     print("Game board:")
-#     for row in table:
-#         for value in row:
-#             print("{}".format(value), end=" ")
-#         print()
+def print_board (table):
+    print("Game board:")
+    for row in table:
+        for value in row:
+            print("{}".format(value), end=" ")
+        print()
 
 # def draw_board(table,coordinates):
 #     for i in coordinates:
@@ -90,32 +87,61 @@
 #     return table
 # 
 
-def render_board(snake_positions, fruit_position, size=10):
-    board = game_board(size)
-    x, y = fruit_position
-    board[x][y] = "@"
-    for x, y in snake_positions:
-        board[x][y] = "X"
-    return board
+# def draw_board(coordinates,size=10):
+#     table = []
+#     for a in range(size):
+#         row = []
+#         for b in range(size):
+#             row.append(".")
+#         table.append(row)
+#     for i in coordinates:
+#         x_y=i
+#         for j in x_y:
+#             x=x_y[0]
+#             y=x_y[1]
+#         if table[x][y]!="X":
+#             table[x][y]="X" 
+#         else:
+#             raise ValueError
+#     return table
 
-def draw_board(coordinates,size=10):
-    table = []
-    for a in range(size):
-        row = []
-        for b in range(size):
-            row.append(".")
-        table.append(row)
-    for i in coordinates:
-        x_y=i
-        for j in x_y:
-            x=x_y[0]
-            y=x_y[1]
-        if table[x][y]!="X":
-            table[x][y]="X" 
-        else:
-            raise ValueError
-    return table
+# def fruit():
+#     coordinates=[]
+#     for i in range(2):
+#         coordinates.append(randrange(0,3))
+#     return tuple(coordinates)
+
+# def render_board(snake_positions, fruit_position, size=3):
+#     board = game_board(size)
+#     x, y = fruit_position
+#     board[x][y] = "@"
+#     for x, y in snake_positions:
+#         board[x][y] = "X"
+#     return board
 
 
 
-print (draw_board([(0,0),(0,1),(0,2)]))
+# coordinates=[(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1)]
+
+
+# while True:
+#     coordinates_fruit=fruit()
+#     check = coordinates_fruit in coordinates
+#     print (check)
+#     if check:
+#         coordinates_fruit=fruit()
+#         print (coordinates_fruit)
+#     else:
+#         table= render_board(coordinates,coordinates_fruit)
+#         print_board (table)
+#         break
+
+coordinates = [(5, 3), (6, 3), (6, 2)]
+coordinates=[((coordinates[0][0]-1),coordinates[0][1])+coordinates]
+print (coordinates)
+# coordinates_fruit = (6, 2)
+# if coordinates[-1] != coordinates_fruit:
+#     print ("la")
+# else:
+#     print ("la-la")
+
--- snake.py
+++ snake.py
@@ -58,7 +58,30 @@ def movement (coordinates, direction):
             raise ValueError       
         move=(last_spot[0],change)
         coordinates.append(move)
-    coordinates.pop(0)                   
+    coordinates.pop(0) 
+
+def increase (coordinates, direction):
+    last_spot = coordinates[-1]
+    if direction == "s":
+        change=last_spot[0]+1
+        move=(change,last_spot[1])
+        coordinates.append(move)
+    if direction == "w":
+        change=last_spot[0]-1
+        if change == -1:
+            raise ValueError
+        move=(change,last_spot[1])
+        coordinates.append(move)
+    if direction == "d":
+        change=last_spot[1]+1
+        move=(last_spot[0],change)
+        coordinates.append(move) 
+    if direction == "a":
+        change=last_spot[1]-1
+        if change == -1:
+            raise ValueError       
+        move=(last_spot[0],change)
+        coordinates.append(move)                 
 
 def movement_check(coordinates):
     if len(coordinates) == len(set(coordinates)):
@@ -69,8 +92,8 @@ def movement_check(coordinates):
 def fruit():
     coordinates=[]
     for i in range(2):
-        coordinates.append(randrange(0,9))
-    return coordinates
+        coordinates.append(randrange(0,10))
+    return tuple(coordinates)
 
 def fruit_on_board(table, coordinates):
     for i in coordinates:
@@ -85,36 +108,57 @@ def fruit_on_board(table, coordinates):
 
 coordinates=[(0,0),(0,1),(0,2)]
 table=game_board()
-coordinates_fruit=fruit()
-table = render_board (coordinates, coordinates_fruit)
+draw_board(table,coordinates)
 print_board (table)
 
-try:
-    table = fruit_on_board(table,coordinates_fruit)
-    print_board (table)
-except ValueError:
-    print("error")
+while True:
     coordinates_fruit=fruit()
-    fruit_on_board(table,coordinates_fruit) 
-    print_board(table)
-
-for i in range(20):
-    player_move=input (
-        "Where do you want a snake to move now?\n\
-        Chose between a - left, d - right, w - up, s - down\n\
-        or end - to leave the game. ")
-    if player_move == "end":
-        break
-    try:
-        movement(coordinates, player_move)
-    except ValueError:
-        print ("Oh no! You crashed against the wall. You lost!")
-        break
-    
-    table = render_board(coordinates,coordinates_fruit)
-    result = movement_check (coordinates)
-    if result:
-        print ("Oh no! You are bitting yourself. You lost!")
-        break
-
-    print_board (table)
+    check = coordinates_fruit in coordinates
+    print (check)
+    if check:
+        coordinates_fruit=fruit()
+        print (coordinates_fruit)
+    else:
+        while coordinates[-1] != coordinates_fruit:
+            player_move=input (
+                "Where do you want a snake to move now?\n\
+                Chose between a - left, d - right, w - up, s - down\n\
+                or end - to leave the game. ")
+            if player_move == "end":
+                break
+            try:
+                movement(coordinates, player_move)
+            except ValueError:
+                print ("Oh no! You crashed against the wall. You lost!")
+                break
+        
+            table = render_board(coordinates,coordinates_fruit)
+            result = movement_check (coordinates)
+            if result:
+                print ("Oh no! You are bitting yourself. You lost!")
+                break
+            print_board (table)
+            print (coordinates)
+            print (coordinates_fruit)
+        else:
+            print ("Yammy! It is good for me!")
+            player_move=input (
+                "Where do you want a snake to move now?\n\
+                Chose between a - left, d - right, w - up, s - down\n\
+                or end - to leave the game. ")
+            if player_move == "end":
+                break
+            try:
+                increase(coordinates, player_move)
+            except ValueError:
+                print ("Oh no! You crashed against the wall. You lost!")
+                break
+        
+            table = render_board(coordinates,coordinates_fruit)
+            result = movement_check (coordinates)
+            if result:
+                print ("Oh no! You are bitting yourself. You lost!")
+                break
+            print_board (table)
+            print (coordinates)
+            print (coordinates_fruit)
